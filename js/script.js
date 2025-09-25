366ec88310db770171cf47af522e2f5613c79212
Joseph001479 <joseaparecido253516@gmail.com>
2025-09-24 22:10:59 -0300

    Delete js directory

--- js/script.js
+++
@@ -1,4 +0,0 @@
-// Efeito na barra de pesquisa
-document.querySelector('.search input').addEventListener('focus', ()=>{
-  document.querySelector('.search').style.boxShadow = '0 6px 20px rgba(0,0,0,0.6)';
-});
